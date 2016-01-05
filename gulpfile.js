bb139df0374509267860648920ec854e05cd4f7b
taoyuan <torworx@gmail.com>
2016-01-05 17:32:02 +0800

    Upgraded dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -53,5 +53,5 @@ gulp.task('coveralls', ['test'], function () {
     .pipe(coveralls());
 });
 
-gulp.task('prepublish', ['nsp']);
+gulp.task('prepublish', [/*'nsp'*/]);
 gulp.task('default', ['static', 'test', 'coveralls']);
