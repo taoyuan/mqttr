6dbe296c904168a74c4bea1de5a78e34d609441d
towyuan <towyuan@outlook.com>
2017-10-12 15:28:24 +0800

    Upgrade dependencies and format codes

--- bin/pub.js
+++ bin/pub.js
@@ -1,128 +1,126 @@
 #!/usr/bin/env node
-"use strict";
 
-var mqttr = require('../');
-var path = require('path');
-var fs = require('fs');
-var concat = require('concat-stream');
-var minimist = require('minimist');
+const mqttr = require('../');
+const path = require('path');
+const fs = require('fs');
+const concat = require('concat-stream');
+const minimist = require('minimist');
 
-var helpMe = require('help-me')({
-  dir: path.join(__dirname, '..', 'doc')
+const helpMe = require('help-me')({
+	dir: path.join(__dirname, '..', 'doc')
 });
 
 function send(args) {
-  var client = mqttr.connect(args);
-  client.on('connect', function () {
-    var message = args.message;
-    try {
-      message = JSON.parse(message);
-    } catch (e) {
-      // no-op
-    }
-    client.publish(args.topic, message, args);
-    client.end();
-  });
+	const client = mqttr.connect(args);
+	client.on('connect', () => {
+		let message = args.message;
+		try {
+			message = JSON.parse(message);
+		} catch (e) {
+			// No-op
+		}
+		client.publish(args.topic, message, args);
+		client.end();
+	});
 }
 
 function start(args) {
-  args = minimist(args, {
-    string: ['hostname', 'username', 'password', 'key', 'cert', 'ca', 'message'],
-    integer: ['port', 'qos'],
-    boolean: ['stdin', 'retain', 'help', 'insecure'],
-    alias: {
-      port: 'p',
-      hostname: ['h', 'host'],
-      topic: 't',
-      message: 'm',
-      qos: 'q',
-      clientId: ['i', 'id'],
-      retain: 'r',
-      username: 'u',
-      password: 'P',
-      stdin: 's',
-      protocol: ['C', 'l'],
-      msgpack: 'M',
-      help: 'H',
-      ca: 'cafile'
-    },
-    default: {
-      host: 'localhost',
-      qos: 0,
-      retain: false,
-      topic: ''
-    }
-  });
+	args = minimist(args, {
+		string: ['hostname', 'username', 'password', 'key', 'cert', 'ca', 'message'],
+		integer: ['port', 'qos'],
+		boolean: ['stdin', 'retain', 'help', 'insecure'],
+		alias: {
+			port: 'p',
+			hostname: ['h', 'host'],
+			topic: 't',
+			message: 'm',
+			qos: 'q',
+			clientId: ['i', 'id'],
+			retain: 'r',
+			username: 'u',
+			password: 'P',
+			stdin: 's',
+			protocol: ['C', 'l'],
+			msgpack: 'M',
+			help: 'H',
+			ca: 'cafile'
+		},
+		default: {
+			host: 'localhost',
+			qos: 0,
+			retain: false,
+			topic: ''
+		}
+	});
 
-  if (args.help) {
-    return helpMe.toStdout('publish');
-  }
+	if (args.help) {
+		return helpMe.toStdout('publish');
+	}
 
-  if (args.key) {
-    args.key = fs.readFileSync(args.key);
-  }
+	if (args.key) {
+		args.key = fs.readFileSync(args.key);
+	}
 
-  if (args.cert) {
-    args.cert = fs.readFileSync(args.cert);
-  }
+	if (args.cert) {
+		args.cert = fs.readFileSync(args.cert);
+	}
 
-  if (args.ca) {
-    args.ca = fs.readFileSync(args.ca);
-  }
+	if (args.ca) {
+		args.ca = fs.readFileSync(args.ca);
+	}
 
-  if (args.key && args.cert && !args.protocol) {
-    args.protocol = 'mqtts';
-  }
+	if (args.key && args.cert && !args.protocol) {
+		args.protocol = 'mqtts';
+	}
 
-  if (!args.msgpack) {
-    args.codec = 'json';
-  }
+	if (!args.msgpack) {
+		args.codec = 'json';
+	}
 
-  if (args.port) {
-    if (typeof args.port !== 'number') {
-      console.warn('# Port: number expected, \'%s\' was given.', typeof args.port);
-      return;
-    }
-  }
+	if (args.port) {
+		if (typeof args.port !== 'number') {
+			console.warn('# Port: number expected, \'%s\' was given.', typeof args.port);
+			return;
+		}
+	}
 
-  if (args['will-topic']) {
-    args.will = {};
-    args.will.topic = args['will-topic'];
-    args.will.payload = args['will-message'];
-    args.will.qos = args['will-qos'];
-    args.will.retain = args['will-retain'];
-  }
+	if (args['will-topic']) {
+		args.will = {};
+		args.will.topic = args['will-topic'];
+		args.will.payload = args['will-message'];
+		args.will.qos = args['will-qos'];
+		args.will.retain = args['will-retain'];
+	}
 
-  if (args.insecure) {
-    args.rejectUnauthorized = false;
-  }
+	if (args.insecure) {
+		args.rejectUnauthorized = false;
+	}
 
-  args.topic = args.topic || args._.shift();
-  args.message = args.message || args._.shift() || '';
+	args.topic = args.topic || args._.shift();
+	args.message = args.message || args._.shift() || '';
 
+	if (!args.topic) {
+		console.error('missing topic\n');
+		return helpMe.toStdout('publish');
+	}
+	args.topic = args.topic.toString();
 
-  if (!args.topic) {
-    console.error('missing topic\n');
-    return helpMe.toStdout('publish');
-  }
-  args.topic = args.topic.toString();
+	if (typeof args.message !== 'string') {
+		args.message = args.message.toString().trim();
+	}
 
-  if (typeof args.message !== 'string') {
-    args.message = args.message.toString().trim();
-  }
-
-  if (args.stdin) {
-    process.stdin.pipe(concat(function (data) {
-      args.message = data.toString().trim();
-      send(args);
-    }));
-  } else {
-    send(args);
-  }
+	if (args.stdin) {
+		process.stdin.pipe(concat((data) => {
+			args.message = data.toString().trim();
+			send(args);
+		}));
+	} else {
+		send(args);
+	}
 }
 
 module.exports = start;
 
 if (require.main === module) {
-  start(process.argv.slice(2));
+	start(process.argv.slice(2));
 }
--- bin/sub.js
+++ bin/sub.js
@@ -1,124 +1,123 @@
 #!/usr/bin/env node
-"use strict";
 
-var util = require('util');
-var path = require('path');
-var fs = require('fs');
-var minimist = require('minimist');
-var mqtt = require('../');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
+const minimist = require('minimist');
+const mqtt = require('../');
 
-var helpMe = require('help-me')({
-  dir: path.join(__dirname, '..', 'doc')
+const helpMe = require('help-me')({
+	dir: path.join(__dirname, '..', 'doc')
 });
 
 function start(args) {
-  args = minimist(args, {
-    string: ['hostname', 'username', 'password', 'key', 'cert', 'ca'],
-    integer: ['port', 'qos', 'keepAlive'],
-    boolean: ['stdin', 'help', 'clean', 'insecure'],
-    alias: {
-      port: 'p',
-      hostname: ['h', 'host'],
-      topic: 't',
-      qos: 'q',
-      clean: 'c',
-      keepalive: 'k',
-      clientId: ['i', 'id'],
-      username: 'u',
-      password: 'P',
-      protocol: ['C', 'l'],
-      msgpack: 'M',
-      verbose: 'v',
-      help: '-H',
-      ca: 'cafile'
-    },
-    default: {
-      host: 'localhost',
-      qos: 0,
-      retain: false,
-      clean: true,
-      keepAlive: 30 // 30 sec
-    }
-  });
+	args = minimist(args, {
+		string: ['hostname', 'username', 'password', 'key', 'cert', 'ca'],
+		integer: ['port', 'qos', 'keepAlive'],
+		boolean: ['stdin', 'help', 'clean', 'insecure'],
+		alias: {
+			port: 'p',
+			hostname: ['h', 'host'],
+			topic: 't',
+			qos: 'q',
+			clean: 'c',
+			keepalive: 'k',
+			clientId: ['i', 'id'],
+			username: 'u',
+			password: 'P',
+			protocol: ['C', 'l'],
+			msgpack: 'M',
+			verbose: 'v',
+			help: '-H',
+			ca: 'cafile'
+		},
+		default: {
+			host: 'localhost',
+			qos: 0,
+			retain: false,
+			clean: true,
+			keepAlive: 30 // 30 sec
+		}
+	});
 
-  if (args.help) {
-    return helpMe.toStdout('subscribe');
-  }
+	if (args.help) {
+		return helpMe.toStdout('subscribe');
+	}
 
-  args.topic = args.topic || args._.shift();
+	args.topic = args.topic || args._.shift();
 
-  if (!args.topic) {
-    console.error('missing topic\n');
-    return helpMe.toStdout('subscribe');
-  }
+	if (!args.topic) {
+		console.error('missing topic\n');
+		return helpMe.toStdout('subscribe');
+	}
 
-  if (args.key) {
-    args.key = fs.readFileSync(args.key);
-  }
+	if (args.key) {
+		args.key = fs.readFileSync(args.key);
+	}
 
-  if (args.cert) {
-    args.cert = fs.readFileSync(args.cert);
-  }
+	if (args.cert) {
+		args.cert = fs.readFileSync(args.cert);
+	}
 
-  if (args.ca) {
-    args.ca = fs.readFileSync(args.ca);
-  }
+	if (args.ca) {
+		args.ca = fs.readFileSync(args.ca);
+	}
 
-  if (args.key && args.cert && !args.protocol) {
-    args.protocol = 'mqtts';
-  }
+	if (args.key && args.cert && !args.protocol) {
+		args.protocol = 'mqtts';
+	}
 
-  if (args.insecure) {
-    args.rejectUnauthorized = false;
-  }
+	if (args.insecure) {
+		args.rejectUnauthorized = false;
+	}
 
-  if (!args.msgpack) {
-    args.codec = 'json';
-  }
+	if (!args.msgpack) {
+		args.codec = 'json';
+	}
 
-  if (args.port) {
-    if (typeof args.port !== 'number') {
-      console.warn('# Port: number expected, \'%s\' was given.', typeof args.port);
-      return;
-    }
-  }
+	if (args.port) {
+		if (typeof args.port !== 'number') {
+			console.warn('# Port: number expected, \'%s\' was given.', typeof args.port);
+			return;
+		}
+	}
 
-  if (args['will-topic']) {
-    args.will = {};
-    args.will.topic = args['will-topic'];
-    args.will.payload = args['will-message'];
-    args.will.qos = args['will-qos'];
-    args.will.retain = args['will-retain'];
-  }
+	if (args['will-topic']) {
+		args.will = {};
+		args.will.topic = args['will-topic'];
+		args.will.payload = args['will-message'];
+		args.will.qos = args['will-qos'];
+		args.will.retain = args['will-retain'];
+	}
 
-  args.keepAlive = args['keep-alive'];
+	args.keepAlive = args['keep-alive'];
 
-  var client = mqtt.connect(args);
+	const client = mqtt.connect(args);
 
-  client.on('connect', function () {
-    client.subscribe(args.topic, function (topic, payload) {
-      if (Buffer.isBuffer(payload)) {
-        payload = payload.toString();
-      }
+	client.on('connect', () => {
+		client.subscribe(args.topic, (topic, payload) => {
+			if (Buffer.isBuffer(payload)) {
+				payload = payload.toString();
+			}
 
-      if (args.verbose) {
-        console.log('[' + topic + ']', util.inspect(payload, {colors: true, depth: 99}));
-      } else {
-        console.log(payload);
-      }
-    }, {qos: args.qos}, function (err, result) {
-      result.forEach(function (sub) {
-        if (sub.qos > 2) {
-          console.error('subscription negated to', sub.topic, 'with code', sub.qos);
-          process.exit(1);
-        }
-      });
-    });
-  });
+			if (args.verbose) {
+				console.log('[' + topic + ']', util.inspect(payload, {colors: true, depth: 99}));
+			} else {
+				console.log(payload);
+			}
+		}, {qos: args.qos}, (err, result) => {
+			result.forEach((sub) => {
+				if (sub.qos > 2) {
+					console.error('subscription negated to', sub.topic, 'with code', sub.qos);
+					process.exit(1);
+				}
+			});
+		});
+	});
 }
 
 module.exports = start;
 
 if (require.main === module) {
-  start(process.argv.slice(2));
+	start(process.argv.slice(2));
 }
